7c9648aa046d305994c7cdbda65f2b27dade1519
Ajinkya Apte <apte.aj@gmail.com>
2025-03-30 22:26:43 -0700

    Dissonity integrated

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # snapser-demo-discord-unity
-Discord Activity Example using Dissonity 
+Discord Activity Example using Dissonity
+
+
+## Setup
+### 1. Install Dissonity
+1. Go to Window > Package Manager > Add package from git URL
+2. Install the package from https://github.com/Furnyr/Dissonity.git?path=/unity#v1
+3. Right click in the hierarchy, Dissonity > Discord Bridge. Add it to the main scene.
+
+You can now build your game for WebGL and put it in your nested iframe. If you're not using the example Node.js project you will need to follow the "Project configuration" guide below.
--- SnapserUnityWeb/Assets/Scripts/Screens/BaseScreen.cs
+++ SnapserUnityWeb/Assets/Scripts/Screens/BaseScreen.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Dissonity.Api;
+
 
 public abstract class BaseScreen : MonoBehaviour
 {
+    async void Start()
+    {
+        string userId = await GetUserId();
+        Debug.Log($"The user's id is {userId}");
+
+        SubActivityInstanceParticipantsUpdate((data) =>
+        {
+            Debug.Log("Received a participants update!");
+        });
+    }
+
     public virtual void Show()
     {
         this.gameObject.SetActive(true);
     }
-    
+
     public virtual void Hide()
     {
         this.gameObject.SetActive(false);
