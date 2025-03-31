22d2f47602687649ecce308cf2a2403956d1a2bc
Ajinkya Apte <apte.aj@gmail.com>
2025-03-30 23:16:57 -0700

    Snapser: Integrated

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Discord Activity Example using Dissonity
 
 
 ## Setup
-### 1. Install Dissonity
+### Install Libraries
+#### 1. Install Dissonity
 1. Go to Window > Package Manager > Add package from git URL
 2. Install the package from https://github.com/Furnyr/Dissonity.git?path=/unity#v1
 
@@ -12,14 +13,28 @@ After the installation is done,
 
 You can now build your game for WebGL and put it in your nested iframe. If you're not using the example Node.js project you will need to follow the "Project configuration" guide below.
 
-### 2. Install NuGet as it makes it easier to add other dependencies
+#### 2. Install NuGet as it makes it easier to add other dependencies
 1. 1. Go to Window > Package Manager > Add package from git URL
 2. Install package from https://github.com/GlitchEnzo/NuGetForUnity.git?path=/src/NuGetForUnity
 
 - You will now see the NuGet button the the top menu
 
-### 3. Install Newtonsoft.Json via Nuget
+#### 3. Install Newtonsoft.Json via Nuget
 1. Click on the NuGet package manager
 2. Search for the following packages:
 - Newtonsoft.Json
-- Unity Test Framework
+- Unity Test Framework (Only if you want to add tests)
+
+### Install Snapser SDK
+#### 1. Download the SDK
+1. Go to your Snapend home page and find the Client SDK download widget.
+2. Select Unity C# for the Platform and pick **unitywebrequest** for the HTTP type.
+3. Click generate.
+
+You will now get a Zip file that you should save it to your machine.
+
+#### 2. Integrate the Snapser SDK
+1. After opening the Zip file you should see a `src/Snapser` folder.
+2. Copy the `src/Snapser` folder and paste it inside Unity repo roots `Assets/Scripts` folder.
+
+- You are now ready to start integrating with your backend.
--- SnapserUnityWeb/Assets/Scripts/Screens/ExampleScreen.cs
+++ SnapserUnityWeb/Assets/Scripts/Screens/ExampleScreen.cs
@@ -2,18 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using static Dissonity.Api;
+using Snapser.Api;
+using Snapser.Client;
+using Snapser.Model;
+
 
 public class ExampleScreen : BaseScreen
 {
     public TextMeshProUGUI TextLabel;
-    
+
+    async void Start()
+    {
+        // Show the Discord user id
+        string userId = await GetUserId();
+        Debug.Log($"The user's id is {userId}");
+
+        SubActivityInstanceParticipantsUpdate((data) =>
+        {
+            Debug.Log("Received a participants update!");
+        });
+
+        // Get an Anonymous user id from Snapser
+        Configuration config = new Configuration();
+        config.BasePath = "https://gateway-accel.snapser.com/q9n92w7o";
+        var apiInstance = new AuthServiceApi(config);
+        var anonLoginRequest = new AuthAnonLoginRequest(createUser: true, username: userId);
+
+        try
+        {
+            // Anonymous Login
+            AuthAnonLoginResponse result = await apiInstance.AnonLoginAsync(anonLoginRequest);
+            Debug.Log(result.User.Id);
+            TextLabel.text = result.User.Id;
+            Debug.Log("Done!");
+        }
+        catch (ApiException e)
+        {
+            Debug.LogError("Exception when calling AuthServiceApi.AnonLogin: " + e.Message);
+            Debug.LogError("Status Code: " + e.ErrorCode);
+            Debug.LogError(e.StackTrace);
+            TextLabel.text = "Unable to login";
+        }
+    }
+
+
     public override void Show()
     {
         base.Show();
-
-        TextLabel.text = "Example";
     }
-    
+
     public void OnBackClicked()
     {
         UIManager.Instance.ShowScreen(UIManager.Instance.MainMenuScreen);
